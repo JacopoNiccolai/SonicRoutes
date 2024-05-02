5e18a119d1da846ada8d69b9416b2fc5ae28b84b
Giulio Bello <giuliobello12@gmail.com>
2024-05-02 10:46:20 +0200

    Aggiornamento delle versioni di AGP, Kotlin e Gson

--- app/src/main/java/com/unipi/dii/sonicroutes/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/unipi/dii/sonicroutes/ui/dashboard/DashboardViewModel.kt
@@ -1,5 +1,6 @@
 package com.unipi.dii.sonicroutes.ui.dashboard
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
