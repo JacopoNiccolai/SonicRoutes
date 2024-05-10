4d6ed116cbcedad23d6bea96da9267dc0153348d
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2024-05-10 13:19:01 +0200

    added edge logic

--- app/src/main/java/com/unipi/dii/sonicroutes/model/Edge.kt
+++ app/src/main/java/com/unipi/dii/sonicroutes/model/Edge.kt
@@ -2,8 +2,8 @@ package com.unipi.dii.sonicroutes.model
 
 
 // the  edge connects two crossings with noise as a weight and the number of measurements
-class Edge(private val startCrossingId: Int, private val endCrossingId: Int, private val noise: Double, private val measurements: Int) {
+class Edge(private val startCrossingId: Int, private val endCrossingId: Int, private val amplitude: Double, private val measurements: Int) {
     override fun toString(): String {
-        return "Edge(start=$startCrossingId, end=$endCrossingId, noise=$noise, measurements=$measurements)"
+        return "Edge(start=$startCrossingId, end=$endCrossingId, noise=$amplitude, measurements=$measurements)"
     }
 }
