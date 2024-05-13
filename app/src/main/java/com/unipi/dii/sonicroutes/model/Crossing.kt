dabb0e8d832c911bc720cedd3d9990827bec5937
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2024-05-13 15:34:42 +0200

    forgot private declaration

--- app/src/main/java/com/unipi/dii/sonicroutes/model/Crossing.kt
+++ app/src/main/java/com/unipi/dii/sonicroutes/model/Crossing.kt
@@ -2,7 +2,7 @@ package com.unipi.dii.sonicroutes.model
 
 import com.google.android.gms.maps.model.LatLng
 
-data class Crossing(val id: Int, val coordinates: LatLng, val streetName: List<String>) {
+data class Crossing(private val id: Int,private val coordinates: LatLng,private val streetName: List<String>) {
     override fun toString(): String {
         return "Crossing(coordinates=($coordinates), streetName='$streetName')"
     }
@@ -15,7 +15,7 @@ data class Crossing(val id: Int, val coordinates: LatLng, val streetName: List<S
         return ArrayList(streetName)
     }
 
-    fun getCrossingId(): Int {
+    fun getId(): Int {
         return id
     }
 }
